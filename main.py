25b958366aea907931c2dea2cf281a456c31fbe5
Lucas Claro <31224118+LucasClaroNicolau@users.noreply.github.com>
2019-06-07 16:57:50 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_restful import Api
+from flask_restful.representations import json
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
+
+@app.route('/contatos', methods=['GET'])
+def contatos():
+    response = [{
+        "nome":"Lucas Claro",
+        "numero":"48998385996"
+    },
+    {
+        "nome": "Carolina Pereira",
+        "numero": "48999101231"
+    }]
+    return jsonify(response)
 
 
 if __name__ == "__main__":
