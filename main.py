f7abbf766caf173e7f7e5ceecf1345e649ef2353
Claro <Lucas.Claro@thomsonreuters.com>
2019-06-13 17:09:51 -0300

    Metodo Get Completo, Começo do Método Post
    Metodo Get Completo, Começo do Método Post

--- main.py
+++ main.py
@@ -1,47 +1,38 @@
-#print(request.remote_addr) comando que pega o ip do cliente
+import json
+
 import MySQLdb
 from flask import Flask, render_template, jsonify, request
 from flask_restful import Api
-from mysql.connector import (connection)
-from flask_restful.representations import json
 
 app = Flask(__name__)
 api = Api(app)
 cnx = MySQLdb.connect(host="127.0.0.1",port=3306,user="root",passwd="admin",db="rede3sunesc")
 conx = cnx.cursor()
-conx.execute("CREATE TABLE IF NOT EXISTS contatos (nome VARCHAR(255), numero INT)")
+conx.execute("CREATE TABLE IF NOT EXISTS contatos (nome VARCHAR(255), numero VARCHAR(18))")
 
-@app.route("/adccontato")
+@app.route("/adccontato", methods=['POST'])
 def setContato():
+    dados = request.data
+    insert = json.loads(dados.decode('utf8').replace("'", '"'))
     sql = "INSERT INTO contatos (nome, numero) VALUES (%s, %s)"
-    val = ("John", "321")
+    val = (""+insert.get('nome'), ""+insert.get('nome'))
     conx.execute(sql,val)
     cnx.commit()
     return ""
 
-@app.route("/listcontatos")
+@app.route("/listcontatos", methods=['GET'])
 def getContato():
-    print(conx.execute("SELECT * FROM contatos;"))
-    return "";
+    conx.execute("SELECT * FROM contatos;")
+    contatos = conx.fetchall()
+    response = []
+    for t in contatos:
+        o = { "nome": t[0], "numero": str(t[1])}
+        response.append(o)
+    return jsonify(response)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/contatos', methods=['GET'])
-def contatos():
-    response = [{
-        "nome":"Lucas Claro",
-        "numero":"48998385996"
-    },
-    {
-        "nome": "Carolina Pereira",
-        "numero": "48999101231"
-    }]
-    return jsonify(response)
-
-
 if __name__ == "__main__":
-    setContato()
-    #getContato()
-#   app.run(host='0.0.0.0', port=2222)
+   app.run(host='0.0.0.0', port=2222)
--- static/index-controller.js
+++ static/index-controller.js
@@ -5,15 +5,49 @@ agenda.config(function($mdThemingProvider) {
     .primaryPalette('blue');
 })
 
-agenda.controller('Tabela', function($scope,$http) {
-    $http.get('/contatos').then(function(response){
+agenda.controller('Tabela', function($scope,$http,$mdDialog) {
+
+    $http.get('/listcontatos').then(function(response){
         $scope.pessoas = response.data
         console.log(response.data);
     }, function(error){
         console.log(error);
     });
 
+    $scope.showAdvanced = function(ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'static/adicionar.html',
+            targetEvent: ev,
+        })
+    };
+
+    function DialogController($scope, $mdDialog,$http ) {
+        $scope.cancelar = function() {
+            $mdDialog.hide();
+        }
+
+        $scope.add = function() {
+            console.log($scope.nomeadd);
+            console.log($scope.numeroadd);
+            data = {
+                "nome":""+$scope.nomeadd,
+                "numero" : $scope.numeroadd
+            }
+            $http.post('/adccontato', data).then(function successCallback(response) {
+                $http.get('/listcontatos').then(function(response){
+                $scope.pessoas = response.data
+            }, function(error){
+                console.log(error);
+            });
+            }, function errorCallback(response) {
+
+            });
+            $mdDialog.hide();
+        }
+     }
 });
+
 
 
 agenda.controller('Index', function($scope) {
